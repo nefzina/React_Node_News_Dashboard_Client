6ea1e6f9d6ca27ea535c714c21618e238b98dd6d
nefzina <nefzi.amani@gmail.com>
2025-09-30 14:51:30 +0200

    feat: pagination of articles displayed from each category
    - Add pagination component
    - Add pagination style file
    - Modify app file to only display a selection of articles per page

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -2,7 +2,7 @@
 
 import { useState } from "react";
 import type { NewsCategory } from "../App";
-import "./navigation.scss";
+import "./scss/navigation.scss";
 
 interface NavigationProps {
   activeCategory: NewsCategory;
